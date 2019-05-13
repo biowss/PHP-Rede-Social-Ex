a584118912b4d41e16db347eee2b466ca7cc5516
biowss <felipe-5502@hotmail.com>
2019-05-13 20:14:04 -0300

    Add files via upload

--- web/PHP/welcome.php
+++ web/PHP/welcome.php
@@ -6,7 +6,7 @@
 	if($_POST != NULL){
 
 		include_once "connect_db.php";
-
+		
 		$usuario = $_POST["usuario"];
 		$senha = $_POST["senha"];
 
@@ -58,41 +58,54 @@
 
 <!DOCTYPE html>
 
-
-
 <html>
 
 	<head>
-		<title>Ren Chon</title>
+		<title>TITLE</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="../CSS/login-style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</head>
 
+
+
+
+
 	<body>
-		<div class="container">
-			<content id="login-sidebar">
-				<table class="login-table" >
-					<tr>
-						<th><img src="https://pm1.narvii.com/6274/91e724ccdf21f9002929ae41d560fdcc09a0d3bc_hq.jpg" width="30%"> <h2> Rede da Ren-Chon </h2></th>
-					</tr>
-					<tr>
-						<td>
-							<div class="container">
-								<br><br><br><br><br><br>
-								<form method="POST">
-								  	<input type="text" name="usuario" placeholder="usuario"><br><br>
-								  	<input type="password" name="senha" placeholder="senha"><br><br>
-								  	<input type="submit" value="Login">
-								</form><br>
-								<a href="cadastro.php">Cadastrar</a>
-								<a href="">Esqueceu a senha?</a>
-							</div>
-						</td>	
-					</tr>
-				</table>
-			</content>
+
+	<nav class="navbar navbar-expand-lg">
+	  <a class="navbar-brand" href="#"><img src="https://pm1.narvii.com/6274/91e724ccdf21f9002929ae41d560fdcc09a0d3bc_hq.jpg"> Ren-Chon Network </a>
+	</nav>
+
+	<div class="login-sidebar">
+		<div class="login-form">
+            <form method="POST">
+            	<div class="form-group">
+            		<label for="user">User:</label>
+			  		<input type="text" id="user" name="usuario" placeholder="usuario" class="form-control">
+			  	</div>
+			  	<div class="form-group">
+			  		<label for="password">Password:</label>
+			  		<input type="password" id="password" name="senha" placeholder="senha" class="form-control">
+			  	</div>
+			  	<button type="submit" class="btn btn-primary">Login</button>
+			</form>
+
+			<br>
+            
+            <a href="cadastro.php">Cadastrar</a> 
+            
+            <br>
+            
+            <a href="">Esqueceu a senha?</a>
+		</div>
 	</div>
 
-
+	<footer>
+		<p>© 2019 Copyright, AppleHead Company</p>
+	</footer>
 	</body>
 </html>
